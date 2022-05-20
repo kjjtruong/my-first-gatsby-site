250fa4853059c580c387d3a4efb351115db15751
Ken Truong <kjjtruong@gmail.com>
2022-05-20 10:31:48 -0600

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     title: `my-first-gatsby-site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
